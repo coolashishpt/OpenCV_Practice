60e89bcaaa64d6d5bc09c608c5e21b6b10c3762a
coolashishpt <40580290+coolashishpt@users.noreply.github.com>
2020-06-11 19:30:49 +0530

    Add Code

--- cv2_camera.py
+++ cv2_camera.py
@@ -2,8 +2,12 @@ import cv2 as cv
 
 # capture
 cam = cv.VideoCapture(0)
+fourcc = cv.VideoWriter_fourcc(*"XVID")
+out = cv.VideoWriter("Saved.avi", fourcc, 20.0, (640, 480))
 while True:
     ret, frame = cam.read()
+    cv.write(frame)
+    
     cv.imshow("Camera", frame)
     if cv.waitkey(1) & 0xFF == ord("q"):
         break
