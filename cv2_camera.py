b8a2c21f290e2c377498dc4c2e8dd1af4e989514
coolashishpt <40580290+coolashishpt@users.noreply.github.com>
2020-06-11 19:33:58 +0530

    Code Add

--- cv2_camera.py
+++ cv2_camera.py
@@ -8,6 +8,10 @@ while True:
     ret, frame = cam.read()
     cv.write(frame)
     
+    
+    print(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
+    print(cam.get(cv.CAP_PROP_FRAME_WIDTH))
+    
     cv.imshow("Camera", frame)
     if cv.waitkey(1) & 0xFF == ord("q"):
         break
