77db46517abbcfa6add58aa64bc3fd42175ceb44
coolashishpt <40580290+coolashishpt@users.noreply.github.com>
2020-06-19 01:33:08 +0530

    used matplotlib for better vision

--- cv2_adaptive_thresholding.py
+++ cv2_adaptive_thresholding.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 img = cv.imread("sudoku.png", 0)
 
@@ -7,13 +8,21 @@ th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY
 
 th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
 
+title = ["Original", "Thresh Binary", "Adaptive Mean", "Adaptive Gaussian"]
+images = [img, th1, th2, th3]
+
+for i in range(len(images)):
+    plt.subplot(2, 2, i+1)
+    plt.imshow(images[i], "gray")
+    plt.title(title[i])
+    plt.xticks([]), plt.yticks([])
 
 
+plt.show()
+# cv.imshow("Image", img)
+# cv.imshow("th1", th1)
+# cv.imshow("th2", th2)
+# cv.imshow("th3", th3)
 
-cv.imshow("Image", img)
-cv.imshow("th1", th1)
-cv.imshow("th2", th2)
-cv.imshow("th3", th3)
-
-cv.waitKey(0)
-cv.destroyAllWindows()
+# cv.waitKey(0)
+# cv.destroyAllWindows()
