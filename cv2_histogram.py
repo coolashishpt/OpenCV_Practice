2f7773d6906e4dbcd31c57265eac5cacbd25522d
coolashishpt <prasadashish24@gmail.com>
2020-06-26 23:49:07 +0530

    code for histogram

---
+++ cv2_histogram.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+img = cv.imread("lena.jpg")
+
+
+
+hist = cv.calcHist([img], [0], None, [256], [0, 256])
+
+plt.plot(hist)
+
+plt.show()
+
+
+
+
+
+cv.waitKey(0)
+CV.destroyAllWindows()
